21a1acab70d48e71093cba92e860f506116979bb
gaurav.davda <gauravdavda34@gmail.com>
2024-09-29 11:32:48 +0530

    Js: Operators

--- js_3_Operators.js
+++ js_3_Operators.js
@@ -1,14 +1,84 @@
 var a = 10;
-var b = '10';
+var b = "10";
 
 // E.g. '6' and 6 are same here it only check for Values
 console.log("(a == b) => ");
-result = (a == b);
+result = a == b;
 console.log(result);
 // console.log(linebreak);
 
 // E.g. '6' and 6 are same here it  check for Values as well as their Data types
 console.log("(a === b) => ");
-result = (a === b);
+result = a === b;
 console.log(result);
 // console.log(linebreak);
+
+let a = 5;
+let b = '5';
+
+console.log(a == b);  // Output: true (only compares value)
+console.log(a === b); // Output: false (compares value and type)
+
+console.log(a > 3);   // Output: true
+console.log(a <= 5);  // Output: true
+
+
+//---------------------------- Truthy And Falsy Operator ----------------------------
+// Truthy Value : It is a Value that is considered true when encountered in a Boolean Context.
+// Anything that isn't falsy is truthy.
+// This means values like numbers other than 0, non-empty strings, objects, arrays, and more are all truthy.
+if (true) {
+  console.log("This will run."); // `true` is truthy
+}
+
+if (42) {
+  console.log("42 is truthy."); // Any non-zero number is truthy
+}
+
+if ("hello") {
+  console.log("'hello' is truthy."); // Non-empty strings are truthy
+}
+
+if ([]) {
+  console.log("Empty array is truthy."); // Even an empty array is truthy
+}
+
+if ({}) {
+  console.log("Empty object is truthy."); // Empty objects are also truthy
+}
+
+let admin = {
+    name:null
+};
+if (admin.name) {
+  console.log("Name is set.");
+} else {
+  console.log("Name is not set.");
+}
+
+//Falsy : It is a Value that is considered FALSE when encountered in a Boolean Context.
+/* In JavaScript, there are only 6 falsy values:
+   Falsy values: false, 0, -0, "", null, undefined, NaN.*/
+if (false) {
+  console.log("This won't run."); // This block will not execute
+}
+
+if (0) {
+  console.log("This won't run either."); // `0` is falsy, so this doesn't execute
+}
+
+if ("") {
+  console.log("Empty string is falsy."); // Empty string is falsy, so it won't run
+}
+
+if (null) {
+  console.log("This won't run."); // `null` is falsy
+}
+
+if (undefined) {
+  console.log("This won't run."); // `undefined` is falsy
+}
+
+if (NaN) {
+  console.log("This won't run."); // `NaN` is falsy
+}
--- js_4_Loops.js
+++ js_4_Loops.js
@@ -22,73 +22,77 @@ Postdecrement : a--
 2. then Increment a = a-1 */
 
 // ----------For Loop ------------
-for (let i=1 ; i< 11; i++)
-{
-    console.log("Number:",i," Square: ", i**2)
+const arr = [1, 2, 3, 4, 5];
+for (let i = 1; i < 11; i++) {
+  console.log("Number:", i, " Square: ", i ** 2);
 }
 
+// A.  ++i (Pre-Increment)
+for (let i = 0; i < arr.length; ++i) {
+  //statment 3 Execute After everyBody Called
+  console.log("Item", i, " ", arr[i]);
+}
 
 //----------For in ------------
 // - It used to print KEYS of the objects. Only for iterables obj. like string ,arr
+//   It is used to print element index forthe ARRAY.
 studList = {
-    'Gaurav' : 34,
-    'Kunal' : 45,
-    'Neel' : 40,
-    'Bhavesh' : 50
+  Gaurav: 34,
+  Kunal: 45,
+  Neel: 40,
+  Bhavesh: 50,
+};
+
+for (let key in studList) {
+  console.log("Marks of", key, "is", studList[key]);
 }
 
-for (let key in studList)
-{
-    console.log("Marks of",key, "is", studList[key])
-
+for (let index in arr) {
+  console.log("Element", index, "is", studList[arr]);
 }
 
 //----------For of ------------
-// - It used to print values of the objects.Only for iterables obj. like string ,arr
+// - It used to print values of the objects / Arr.
+// - Only for iterables obj. like string ,arr
 
-for (let chr of "Gaurav")
-{
-    console.log(chr)
-
+for (let chr of "Gaurav") {
+  console.log(chr);
 }
 
 //---------While Loop ------------
-    // let n = 3
-    // let i = 0 
-    // while (i< n)
-    // {
-    // console.log(i)
-    // i++
-    // console.log(i)
-    // }
-
+// let n = 3
+// let i = 0
+// while (i< n)
+// {
+// console.log(i)
+// i++
+// console.log(i)
+// }
 
 //-------------Do while loop-----------
-let n =2;
-let i=10;
+let n = 2;
+let i = 10;
 
-do{
-    console.log(i)
-    i++
-}while(i < n)
-
+do {
+  console.log(i);
+  i++;
+} while (i < n);
 
 //Iterate over Object
 
 studList = {
-    'Gaurav' : 34,
-    'Kunal' : 45,
-    'Neel' : 40,
-    'Bhavesh' : 50
-}
+  Gaurav: 34,
+  Kunal: 45,
+  Neel: 40,
+  Bhavesh: 50,
+};
 
-console.log(Object.keys(studList))  //Return Arr of all the Keys
+console.log(Object.keys(studList)); //Return Arr of all the Keys
 
-console.log(Object.keys(studList)[2])  //Return specObject.keys(studList)[i]fic keys 
+console.log(Object.keys(studList)[2]); //Return specObject.keys(studList)[i]fic keys
 
-console.log(studList[Object.keys(studList)[2]])  //Return specfic keys 
+console.log(studList[Object.keys(studList)[2]]); //Return specfic keys
 
-for (let i=0; i<Object.keys(studList).length; i++)
-{
-    console.log("Marks of", "is", studList[Object.keys(studList)[i]])
+for (let i = 0; i < Object.keys(studList).length; i++) {
+  console.log("Marks of", "is", studList[Object.keys(studList)[i]]);
 }
