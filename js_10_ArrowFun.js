c9b1b69bacc7fd7f2268a63e63f0c7f9047c853a
gaurav.davda <gauravdavda34@gmail.com>
2024-10-05 13:45:30 +0530

    JS: this Keywords

--- js_10_ArrowFun.js
+++ js_10_ArrowFun.js
@@ -1,42 +1,64 @@
 //Regular Function
 
-function display(){
-    console.log("Hello Guys")
+function display() {
+  console.log("Hello Guys");
 }
 
-display();  
-
+display();
 
 // Method 1 - Arrow Function
-const print = function (){
-    console.log("Hello Guys")
-}
+const print = function () {
+  console.log("Hello Guys");
+};
 
-print();  
+print();
 
 //Method 2 - Arrow Function
 const data = () => {
-    return "Good Mrng"
-}
-console.log(data()) 
+  return "Good Mrng";
+};
+console.log(data());
 
 // One liners do not require braces/return.
 
 //Method 2.1 - Arrow Function
 const greet = () => "Hello Dosto";
-console.log(greet())
+console.log(greet());
 
 //Just for to return any object require ()
-const objData = () => ( { key : "value" } )
-console.log(objData())
- 
-//Pass Arguments 
-const greet_User = (user) => {
-    return "Hello " + user
-}
+const objData = () => ({ key: "value" });
+console.log(objData());
 
-console.log(greet_User("Virat"))
+//Pass Arguments
+const greet_User = (user) => {
+  return "Hello " + user;
+};
+
+console.log(greet_User("Virat"));
 
 //Just for one Arg. / one Statement
-const ask_User_x = name => "How r u " + name + "?"
-console.log(ask_User_x("Gaurav"))
+const ask_User_x = (name) => "How r u " + name + "?";
+console.log(ask_User_x("Gaurav"));
+
+/////////////////////////////////// this Keyword ////////////////////////////
+//  this ==== > Hold Current context
+
+// E.g. 1  this in Browser
+console.log("This: ", this); // Output in Browser : global Obj (Window)
+console.log("This: ", this); // Output by Node : Empty {}
+
+function getData() {
+  console.log("");
+  console.log("This in fun: ", this);
+}
+
+getData();
+
+// this => only works for obj , class
+function getNewData() {
+  const name = "Gaurav ";
+  console.log("");
+  console.log("This in fun: ", this.name); // Output : undefined
+}
+
+getNewData();
