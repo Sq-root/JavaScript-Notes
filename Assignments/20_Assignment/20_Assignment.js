3aa71fdc83ac53c0d70c96c14c664eebfe857eb0
gaurav.davda <gauravdavda34@gmail.com>
2024-11-24 15:48:19 +0530

    JS : HTML Canavs Curve

--- Assignments/20_Assignment/20_Assignment.js
+++ Assignments/20_Assignment/20_Assignment.js
@@ -3,9 +3,11 @@ let canvasctx = canvas.getContext("2d");
 
 if (canvas.getContext) {
   let canvasctx = canvas.getContext("2d");
+
+  // ----------Draw Rectangle------
   //Filed Rectangle
-  // canvasctx.fillStyle = "#3333";
-  // canvasctx.fillRect(10, 10, 50, 50);
+  canvasctx.fillStyle = "#3333";
+  canvasctx.fillRect(130, 30, 50, 50);  // x,y pt where rec start
 
   // //Stroke Rectangle
   // canvasctx.strokeStyle = "red";
@@ -15,15 +17,50 @@ if (canvas.getContext) {
   // canvasctx.clearRect(25, 25, 20, 20);
 
   // ----------Draw Shape------
-  canvasctx.beginPath();
-  canvasctx.moveTo(20, 20);
-  canvasctx.lineTo(20, 50);
-  canvasctx.lineTo(20, 80);
-  canvasctx.lineTo(80, 100);
-  canvasctx.lineTo(100, 20);
-  canvasctx.strokeStyle = "blue";
-  canvasctx.fill();
-  canvasctx.closePath();
+  // canvasctx.beginPath();
+  // canvasctx.moveTo(20, 20);
+  // canvasctx.lineTo(20, 50);
+  // canvasctx.lineTo(80, 20);
+  // canvasctx.lineTo(80, 100);
+  // canvasctx.lineTo(100, 20);
+  // canvasctx.strokeStyle = "blue";
+  // canvasctx.fill();
+  // canvasctx.closePath();
+
+  // ----------Draw Line------
+  // canvasctx.lineWidth = 2;
+  // canvasctx.strokeStyle = "green";
+  // canvasctx.beginPath();
+  // canvasctx.moveTo(5, 5); //Start  Pt
+  // canvasctx.lineTo(100, 5); // End Pt
+  // canvasctx.stroke();
+  // canvasctx.closePath();
+
+  // canvasctx.beginPath();
+  // canvasctx.lineWidth = 3;
+  // canvasctx.strokeStyle = "red";
+  // canvasctx.moveTo(20, 50); //Start  Pt
+  // canvasctx.lineTo(100, 50); // End Pt
+  // canvasctx.stroke();
+  // canvasctx.closePath();
+
+  //--------------QuadraticCurveTo
+  // arc(x, y, radius, startAngle, endAngle, counterclockwise)
+  // canvasctx.beginPath();
+  // canvasctx.lineWidth = 2;
+  // canvasctx.moveTo(50, 50);
+  // canvasctx.quadraticCurveTo(0, 10, 45, 0, 5.78, true);
+  // canvasctx.stroke();
+  // canvasctx.closePath();
+
+  //-----------bezierCurveTo path
+  // context.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
+  // canvasctx.lineWidth = 2;
+  // canvasctx.beginPath();
+  // canvasctx.moveTo(20, 20);
+  // canvasctx.bezierCurveTo(60, 100, 15, 0, 140, 70);
+  // canvasctx.stroke();
+  // canvasctx.closePath();
 } else {
-  console.error("Canvas Failed to get context");
+  console.error("Canvas Failed to get canvasctx");
 }
